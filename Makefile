54c7b6100663440da9f8a1334b6b0e8780584fd4
KokaKiwi <kokakiwi@kokakiwi.net>
2013-11-28 10:44:50 +0100

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,3 @@
+RUST_MODULES		:=	crypto
+
+include				rust-mk/rust.mk
---
+++ src/crypto/lib.rs
@@ -0,0 +1,10 @@
+#[link(
+    name = "crypto",
+    vers = "0.1.0",
+    uuid = "992ef7fe-9b86-4adf-b638-31b6405f2037",
+    package_id = "crypto"
+)];
+
+#[license = "MIT"];
+
+#[crate_type = "lib"];
